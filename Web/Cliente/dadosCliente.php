7f6b18102b021d08e6269bbbbcdfbaa97ab9b842
joaovizu <joaovizu2015@gmail.com>
2017-11-13 21:45:36 -0200

    Version 2.3 - Pagamento Sendo Feito

--- Web/Cliente/dadosCliente.php
+++ Web/Cliente/dadosCliente.php
@@ -1,36 +1,77 @@
-<?php include('Class/Sql.php'); ?>
+<?php include('Class/Sql.php');
+        $cod = $_SESSION['usuario']['CodCliente'];
+?>
 <div class="jumbotron text-center">
     <h1 class="jumbo-text">Meus Dados</h1>
 </div>
 
 <div class="container mt-5">
-        
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Nº Pedido</th>
-                    <th>Data</th>
-                    <th>Valor</th>
-                    <th>Status</th>
-                    <th>Opções</th>
-                </tr>
-            </thead>
 
-            <tbody>
+        <form id="updCli" style="font-size:25px;" role="form" method="post" action="Cliente/updateUser.php?id=<?php echo $cod?>">
+          <div class="box-body">
 
+            <!-- inicio do laço php -->
             <?php
-            $codCli = $_SESSION['usuario']['CodCliente'];
-            $queryCli = mysqli_query($conn, "SELECT * FROM Cliente WHERE CodCliente = '$codCli'");
-            foreach($queryCli as $row):
+                
+                //consulta para pegar o id do cliente que vai ser feito o update
+                $queryCliente = mysqli_query($conn, "SELECT * FROM Cliente a INNER JOIN login b USING(CodCliente) WHERE a.CodCliente = '$cod'");
+                while($row = mysqli_fetch_assoc($queryCliente)){
             ?>
-                <tr>
-                    <td><?php echo $row['CodCliente']?></td>
-                    <td><?php echo $row['DataVenda']?></td>
-                    <td><?php echo $row['TotalEncomenda']?></td>
-                    <td><?php echo $row['StatusPedido']?></td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>    
-    
+            
+            <div class="row">
+              <div class="form-group col-md-9">
+                <label for="desperson">Nome</label>
+                <input type="text" data-cod="<?php echo $cod;?>" class="form-control" id="desperson" name="NomeCliente" placeholder="Digite o nome" value="<?php echo $row['NomeCliente'];?>">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="form-group col-md-6">
+                <label for="deslogin">Login</label>
+                <input type="text" class="form-control" id="deslogin" name="NomeLogin" placeholder="Digite o login"  value="<?php echo $row['NomeLogin'];?>">
+              </div>
+
+              <div class="form-group col-md-6">
+                <label for="desemail">E-mail</label>
+                <input type="email" class="form-control" id="desemail" name="email" placeholder="Digite o e-mail" value="<?php echo $row['email'];?>">
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="form-group col-md-6">
+                <label for="endereco">Endereço</label>
+                <input type="text" class="form-control" id="deslogin" name="endereco" placeholder="Endereço"  value="<?php echo $row['EndCliente'];?>">
+              </div>
+
+              <div class="form-group col-md-2">
+                <label for="">Nº Casa</label>
+                <input type="text" class="form-control" id="numerohouse" name="numCasa" placeholder="Nº da Casa"  value="<?php echo $row['NCliente'];?>">
+              </div>
+
+              <div class="form-group col-md-4">
+                <label for="">Complemento</label>
+                <input type="text" class="form-control" id="complemento" name="complemento" placeholder="Complemento"  value="<?php echo $row['ComCliente'];?>">
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="form-group col-md-6">
+                <label for="nrphone">Telefone</label>
+                <input type="tel" class="form-control" id="nrphone" name="TelefoneCliente" placeholder="Digite o telefone"  value="<?php echo $row['TelefoneCliente'];?>">
+              </div>
+
+              <div class="form-group col-md-6">
+                <label for="nrphone">Celular</label>
+                <input type="tel" class="form-control" id="nrphone" name="CelularCliente" placeholder="Digite o telefone"  value="<?php echo $row['CelularCliente'];?>">
+              </div>
+            </div>           
+
+          </div>
+          <?php } ?>
+          <!-- /.box-body -->
+          <div class="box-footer">
+            <button id="enviar" type="submit" class="btn btn-primary">Alterar Dados</button>
+          </div>
+        </form>
+
 </div>
---
+++ Web/Cliente/updateUser.php
@@ -0,0 +1,35 @@
+<script>
+	function redireciona(){
+		setTimeout("window.location = '../meusDados.php'");
+	}
+</script>
+<?php
+	include('../Class/Sql.php');
+
+	//receber os dados via post
+	$codlogin = $_GET['id'];
+	$nome = $_POST['NomeCliente'];
+	$login = $_POST['NomeLogin'];
+	$telefone = $_POST['TelefoneCliente'];
+	$celular = $_POST['CelularCliente'];
+	$email = $_POST['email'];
+	$endereco = $_POST['endereco'];
+	$complemento = $_POST['complemento'];
+	$numeroC = $_POST['numCasa'];
+	$inadmin = isset($_POST['inadmin']) ? 1:0;
+	
+
+	//query para fazer update
+
+	
+	$query = mysqli_query($conn, "CALL sp_usersupdate_save('$codlogin','$nome','$login','$email','$telefone','$celular','$endereco','$numeroC','$complemento','$inadmin')");
+
+	if($query){
+		echo "Dados do cliente alterado com sucesso";
+		echo "<script>redireciona()</script>";
+	}else{
+		echo "Ocorreu um erro ao alterar os dados do cliente \n".mysqli_errno($conn);
+	}
+
+
+?>
